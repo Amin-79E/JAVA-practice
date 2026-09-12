823b9e0f40e3f0e83fa5f4d7832e996900084c51
BOUHADJAR Mohammed Amin <212972789+Amin-79E@users.noreply.github.com>
2026-09-12 05:06:18 +0100

    Add files via upload

---
+++ MAD_LIBS.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MAD_LIBS {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String adjective2;
+        String adjective3;
+        String noun1;
+        String verb1;
+
+        System.out.print("Enter an adjective: ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun(Animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter another adjective: ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb(Ending with ing): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter another adjective: ");
+        adjective3 = scanner.nextLine();
+        
+        System.out.println("\nToday i went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, i saw a " + noun1 + ".");
+        System.out.println("The" + noun1+ " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+
+        scanner.close();
+    }
+}
