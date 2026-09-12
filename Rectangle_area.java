e58e66028e5c825159e7f9ecfcdb3078ef30e893
BOUHADJAR Mohammed Amin <212972789+Amin-79E@users.noreply.github.com>
2026-09-12 04:26:40 +0100

    Add files via upload

---
+++ Rectangle_area.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Rectangle_area {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    double width = 0;
+    double height = 0;
+    double area = 0;
+
+    System.out.println("This program will calculate the area of a rectangle, based on the inputs you provide.");
+    System.out.print("Enter the width of the rectangle: ");
+    width = scanner.nextDouble();
+
+    System.out.print("Enter the height of the rectangle: ");
+    height = scanner.nextDouble();
+
+    area = height*width;
+
+    System.out.print("The area of the rectangle is: " + area);
+
+    scanner.close();
+    }
+}
