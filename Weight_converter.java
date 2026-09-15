87717353e2d9c23074a6b3d5535b5f940729b4b0
BOUHADJAR Mohammed Amin <212972789+Amin-79E@users.noreply.github.com>
2026-09-15 03:40:38 +0100

    Update Weight_converter.java

--- Weight_converter.java
+++ Weight_converter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class weight_converter{
+public class Weight_converter{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
