a391555cb923406b464401903c9e83f33dfe2dc5
BOUHADJAR Mohammed Amin <212972789+Amin-79E@users.noreply.github.com>
2026-09-19 18:15:06 +0100

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class matrix{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the number of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.println("Enter the number of columns: ");
+        columns = scanner.nextInt();
+
+        System.our.println("Enter the symbol: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int k=0 ; k<rows ; k++){
+        for(int i=0 ; i<columns ; i++){
+          System.out.print(Symbol);
+
+          }
+          System.out.print();
+        }
+
+        scanner.close();
+    } 
+}
